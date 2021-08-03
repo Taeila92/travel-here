dd423e9e0f009b0f1ee1a92068500d2de5a3b460
Taeila92 <kim920125@naver.com>
2021-08-03 19:21:34 +0900

    [Add]category random image 추가

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { dbService } from "firebase.js";
+import { dbService, storageService } from "firebase.js";
 import * as S from "./Category.style";
 const Category = () => {
   const [cate, setCate] = useState([]);
+  const [photo, setPhoto] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const getData = async () => {
       const cateArray = await dbService.collection("post").get();
       const postArray = [];
-      cateArray.forEach((data) => {
-        postArray.push(data.data().post_religion);
+      const imgArray = [];
+
+      cateArray.forEach((res) => {
+        postArray.push(res.data().post_religion);
+        if (res.data().post_photo.length > 0) {
+          const random = Math.floor(
+            Math.random() * res.data().post_photo.length
+          );
+          imgArray.push(res.data().post_photo[random]);
+        }
       });
       postArray.forEach((data, index) => {
         data = data.toLowerCase().trim();
@@ -19,18 +29,35 @@ const Category = () => {
       const set = new Set(postArray);
       const setArray = [...set];
       setCate(setArray);
+      imgArray.forEach(async (cate) => {
+        await storageService
+          .ref(`/post/${cate}`)
+          .getDownloadURL()
+          .then((url) => {
+            setPhoto((prev) => [...prev, url]);
+          });
+        setLoading(true);
+      });
     };
     getData();
   }, []);
+  const click = () => {
+    console.log(loading);
+    console.log(photo);
+  };
   return (
     <S.Container>
       <ul>
         {cate.map((ca, index) => (
           <li key={index}>
-            <S.LinkStyle to={`/categorylist/${ca}`}>{ca}</S.LinkStyle>
+            <S.LinkStyle to={`/categorylist/${ca}`}>
+              <p>{ca}</p>
+              {loading && <img src={photo[index]} width="100px" />}
+            </S.LinkStyle>
           </li>
         ))}
       </ul>
+      <button onClick={click}>click!</button>
     </S.Container>
   );
 };
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,19 +1,18 @@
-import firebase from 'firebase';
-import "firebase/firestore"
-
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID ,
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STOREAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_API_ID,
-  measurementId: process.env.REACT_APP_MEARUEMENT_ID
+  measurementId: process.env.REACT_APP_MEARUEMENT_ID,
 };
-
 
 const firebaseService = firebase.initializeApp(firebaseConfig);
 export const dbService = firebase.firestore();
-
+export const storageService = firebase.storage();
 
 export default firebaseService;

487d1e7038c4b55768b45ed32fe32d1819986555
Elli-Jeon <68575268+Elli-Jeon@users.noreply.github.com>
2021-08-09 11:35:19 +0900

    [Update] 라우팅 이동시에 nav collapse

--- client/src/components/NavLinks/NavLinks.jsx
+++ client/src/components/NavLinks/NavLinks.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { openNav, closeNav } from 'store/modules/nav';
 import * as S from "./NavLinks.style";
 
 
@@ -7,23 +8,32 @@ const activeStyle = { color: "red" };
 
 export default function NavLinks({ isPc }) {
   const {isNavOpened} = useSelector(state => state.nav);
+  const dispatch = useDispatch();
+  
+  const navToggle = () => {
+    if(isNavOpened){
+      dispatch(closeNav())
+    } else {
+      dispatch(openNav())
+    }
+  }
 
   return (
     
     <S.Container isPc={isPc} isNavOpened={isNavOpened}>
       <S.Ul>
         <S.Li>
-          <S.StyledNav to="/" activeStyle={activeStyle} exact>
+          <S.StyledNav onClick={navToggle} to="/" activeStyle={activeStyle} exact>
             Home
           </S.StyledNav>
         </S.Li>
         <S.Li>
-          <S.StyledNav to="/CategoryList" activeStyle={activeStyle} exact>
+          <S.StyledNav onClick={navToggle} to="/CategoryList" activeStyle={activeStyle} exact>
             Blog
           </S.StyledNav>
         </S.Li>
         <S.Li>
-          <S.StyledNav to="/Login" activeStyle={activeStyle} exact>
+          <S.StyledNav onClick={navToggle} to="/Login" activeStyle={activeStyle} exact>
             Login
           </S.StyledNav>
         </S.Li>

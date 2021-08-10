bb55e7dd9fba9a6d72c1202670974057af622ce5
Taeila92 <kim920125@naver.com>
2021-08-10 18:57:23 +0900

    [update] header, navlink 불필요한 코드 삭제 및 수정

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,10 @@
-import Logo from 'components/Logo/Logo';
-import NavLinks from 'components/NavLinks/NavLinks';
-import NavBar from 'components/NavBar/NavBar';
-import * as S from './Header.style';
-import { useEffect, useState } from 'react';
-import useWindowWidth from 'hooks/useWindowWidth';
+import NavLinks from "components/NavLinks/NavLinks";
+import NavBar from "components/NavBar/NavBar";
+import * as S from "./Header.style";
+import { useEffect, useState } from "react";
+import useWindowWidth from "hooks/useWindowWidth";
 
-export default function Header({ onLogout }) {
+export default function Header({ isLoggedIn }) {
   const width = useWindowWidth();
   const [active, setActive] = useState(false);
 
@@ -17,7 +16,7 @@ export default function Header({ onLogout }) {
 
   return (
     <S.Header active={active}>
-      <NavLinks onLogout={onLogout} active={active} />
+      <NavLinks isLoggedIn={isLoggedIn} active={active} />
       <NavBar setActive={setActive} active={active} />
     </S.Header>
   );

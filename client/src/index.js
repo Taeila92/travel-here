dd270e3e774d4b0645c6028a8c6207f8e946f69c
hyunjeongPark <GuoHJP@gmail.com>
2021-08-12 23:49:18 +0900

    [Fix] public/index.html에서 fontawesome 받아오기

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'store';
 import App from 'App';
-import '@fortawesome/fontawesome-free/js/all.js';
 
 ReactDOM.render(
   <React.StrictMode>

fa5fd5de0bf7fc3808d3e5d7c62079b0317df57f
hyunjeongPark <GuoHJP@gmail.com>
2021-08-28 05:44:09 +0900

    [Add] notFound페이지 스타일링

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useHistory } from 'react-router';
+import * as S from "./NotFound.style";
+
 export default function NotFound() {
+
+  const history = useHistory();
+
+  const goBack = () => {
+    history.goBack();
+  }
+
   return (
-    <div>
-      <h2>404</h2>
-      <p>페이지를 찾을수 없습니다.</p>
-    </div>
+    <S.Container>
+      <section>
+        <h1>404</h1>
+        <div>
+          <h2>SORRY!</h2>
+          <p>페이지를 찾을 수 없습니다</p>
+          <button onClick={goBack}>Go Back</button>
+        </div>
+      </section>
+    </S.Container>
   );
 }

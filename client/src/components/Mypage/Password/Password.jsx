b9ed4371bfca63165ec5c9dd65349b11e214dbbf
tkdrl1500 <tkdrl1500@gmail.com>
2021-08-28 17:10:22 +0900

    [Add] 패스워드 변경 초안

---
+++ client/src/components/Mypage/Password/Password.jsx
@@ -0,0 +1,48 @@
+﻿import React, { useState, useRef } from 'react';
+import * as S from './Password.style';
+import firebase from 'firebase';
+
+const Password = () => {
+  function getASecureRandomPassword() {
+    return 'correcthorsebatterystaple';
+  }
+  // [START auth_update_password]
+  const user = firebase.auth().currentUser;
+  const newPassword = getASecureRandomPassword();
+
+  user
+    .updatePassword(newPassword)
+    .then(() => {
+      console.log('Update successful');
+    })
+    .catch((error) => {
+      console.log('An error ocurred');
+    });
+
+  //사용자 재인증
+
+  function promptForCredentials() {
+    return {};
+  }
+  // TODO(you): prompt the user to re-provide their sign-in credentials
+  const credential = promptForCredentials();
+
+  user
+    .reauthenticateWithCredential(credential)
+    .then(() => {
+      console.log('User re-authenticated');
+    })
+    .catch((error) => {
+      console.log('An error ocurred');
+    });
+
+  return (
+    <>
+      <h1>비밀번호 변경</h1>
+      <inpu></inpu>
+      <button>변경</button>
+    </>
+  );
+};
+
+export default Password;
---
+++ client/src/components/Mypage/Password/Password.style.js
@@ -0,0 +1,8 @@
+﻿import styled from 'styled-components';
+
+const Id = styled.li`
+  margin-bottom: 1rem;
+  border: 1px solid black;
+`;
+
+export { Id };

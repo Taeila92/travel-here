8fb893239e29bf0dc85011dc9ddcfa9c661b4915
Taeila92 <kim920125@naver.com>
2021-08-29 18:08:01 +0900

    [Fix] Image remove start

--- client/src/components/Write/WriteModal/WriteModal.jsx
+++ client/src/components/Write/WriteModal/WriteModal.jsx
@@ -131,7 +131,8 @@ export default function WriteModal({ visible, isVisible, postData }) {
   };
 
   const removeAttachment = (e) => {
-    setAttachment([]);
+    console.log(e);
+    // setAttachment([]);
   };
 
   useEffect(() => {
